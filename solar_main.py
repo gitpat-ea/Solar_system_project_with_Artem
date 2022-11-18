6ada695a06e48b84bb3c8aa11aad7ec32d7169fb
gitpat-ea <pateshman.ea@phystech.edu>
2022-11-18 12:09:56 +0300

    tryed commit

--- solar_main.py
+++ solar_main.py
@@ -2,10 +2,10 @@
 # license: GPLv3
 
 import pygame as pg
-from solar_vis import *
-from solar_model import *
-from solar_input import *
-from solar_objects import *
+from source_code.solar_vis import *
+from source_code.solar_model import *
+from source_code.solar_input import *
+from source_code.solar_objects import *
 import thorpy
 import time
 import numpy as np
